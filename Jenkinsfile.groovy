47ad1bcbcf831e5c046b35394f576289edff6fa8
Lionel Tchami <65927599+apotitech@users.noreply.github.com>
2023-05-03 18:55:30 -0600

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,60 @@
+pipeline {
+    agent any
+    environment {
+        PROJECT_ID = 'bold-physics-383802'
+        CLUSTER_NAME = 'cluster-1'
+        LOCATION = 'us-central1-c'
+        CREDENTIALS_ID = 'bold-physics-383802'
+    }
+
+    tools {
+        // Install the Maven version configured as "M3" and add it to the path.
+        maven "maven"
+    }
+
+    stages {
+        stage('SonarQube Analysis') {
+            steps {
+                sh 'sonar-scanner \
+                -Dsonar.host.url=http://10.0.0.77:9000/ \
+                -Dsonar.login=$SQ_Token \
+                -Dsonar.projectKey=my-web-app \
+                -Dsonar.sources=. \
+                -Dsonar.java.binaries=.'
+            }
+        }
+        stage('GIT CHECKOUT') {
+            steps {
+                // Get some code from a GitHub repository
+                git branch: 'main', url: 'https://github.com/apotitech/Hello-Node.git'
+            }
+        }
+        stage('MAVEN BUILD') {
+            steps {
+                // Get some code from a GitHub repository
+                sh "mvn clean install package"
+            }
+        }
+        stage('DOCKER BUILD') {
+            steps {
+                // Get some code from a GitHub repository
+                sh "docker build -t apotieri/app_maven_001 ."
+            }
+        }
+        stage('DOCKER PUSH') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'pwd', usernameVariable: 'user')]) {
+                   sh  "docker login -u ${user} -p ${pwd}"
+                   sh "docker push apotieri/app_maven_001"
+                    }                    
+            }
+        }
+       stage('Deploy to kubernetes'){
+        steps{
+            
+            step([$class: 'KubernetesEngineBuilder', projectId: env.PROJECT_ID, clusterName: env.CLUSTER_NAME, location: env.LOCATION, manifestPattern: 'petclinic.yaml', credentialsId: env.CREDENTIALS_ID, verifyDeployments: true])
+		       echo "Deployment Finished ..."
+        }
+       }
+    }
+}
